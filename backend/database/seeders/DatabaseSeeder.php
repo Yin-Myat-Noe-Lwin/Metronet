ade4c5eedf8e9297af43ff908a09b9eae5f4c011
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-06-29 14:31:49 +0630

    add database seeder file

---
+++ backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Customer;
+use App\Models\IspPlan;
+use App\Models\Cpe;
+use App\Models\ServiceArea;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+        Customer::factory(10)->create();
+
+        IspPlan::factory(5)->create();
+
+        ServiceArea::factory(5)->create();
+
+        Cpe::factory(100)->create();
+    }
+}
