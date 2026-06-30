8da8843e645b855be2f665d3a1ffcf515f108470
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-06-30 21:44:48 +0630

    change update email route to a different file

--- backend/routes/api/auth.php
+++ backend/routes/api/auth.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+    use App\Http\Controllers\Api\AuthController;
+    use App\Http\Controllers\Api\CustomerController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/verify-email', [AuthController::class, 'verifyEmail']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/verify-email', [AuthController::class, 'verifyEmail']);
+    Route::get('/update-email', [CustomerController::class, 'verifyEmail']);
