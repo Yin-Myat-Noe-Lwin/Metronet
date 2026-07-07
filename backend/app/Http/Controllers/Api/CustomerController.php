55c5a3167020fca21a0cfcf83f983f42b662732a
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-07 09:30:09 +0630

    add import in customer controller

--- backend/app/Http/Controllers/Api/CustomerController.php
+++ backend/app/Http/Controllers/Api/CustomerController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cache;
 use PDOException;
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
