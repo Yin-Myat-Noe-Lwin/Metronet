cc7d2d189583b3548440a3631855547011591cae
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-15 12:54:46 +0630

    add jobs schedule

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use App\Jobs\ProcessSubscriptionJob;
 use Illuminate\Support\Facades\Schedule;
+use App\Jobs\SendPaymentReminderJob;
+use App\Jobs\AutoCancelUnpaidSubscriptions;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new SendPaymentReminderJob())
+    ->dailyAt('08:00')
+    ->name('payment-reminder')
+    ->withoutOverlapping();
+
+Schedule::job(new AutoCancelUnpaidSubscriptions())
+    ->dailyAt('10:00')
+    ->name('subscription-cancellation')
+    ->withoutOverlapping();
