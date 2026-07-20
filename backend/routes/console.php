a5c8a46a59734cc0a3045e4ed971b7b90f606268
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-20 09:11:55 +0630

    add create invoice job with schedule

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -3,12 +3,17 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use App\Jobs\CreateInvoiceJob;
 use App\Jobs\SendPaymentReminderJob;
 use App\Jobs\AutoCancelUnpaidSubscriptions;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    CreateInvoiceJob::dispatch();
+})->dailyAt('00:00');
 
 Schedule::job(new SendPaymentReminderJob())
     ->dailyAt('08:00')
@@ -19,3 +24,4 @@ Schedule::job(new AutoCancelUnpaidSubscriptions())
     ->dailyAt('10:00')
     ->name('subscription-cancellation')
     ->withoutOverlapping();
+
