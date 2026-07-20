34113eaad42ceb69dd9ecf8ff0457a42a86bea84
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-20 09:04:58 +0630

    add register service provider

---
+++ backend/app/Providers/RegisterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace App\Providers;
+
+    use Illuminate\Support\ServiceProvider;
+
+    use App\Repositories\CustomerRepository;
+    use App\Repositories\Interfaces\CustomerRepositoryInterface;
+
+    use App\Services\AuthService;
+    use App\Services\Interfaces\AuthServiceInterface;
+
+    class RepositoryServiceProvider extends ServiceProvider
+    {
+        public function register()
+        {
+            $this->app->bind(
+                CustomerRepositoryInterface::class,
+                CustomerRepository::class
+            );
+
+            $this->app->bind(
+                AuthServiceInterface::class,
+                AuthService::class
+            );
+        }
+    }
+?>
