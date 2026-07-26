331c66a541c9997cda1b628b70c3ea62336ce48c
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-27 00:12:32 +0630

    refactor payment consumer command

--- backend/app/Console/Commands/PaymentConsumerCommand.php
+++ backend/app/Console/Commands/PaymentConsumerCommand.php
@@ -6,41 +6,29 @@ use Illuminate\Console\Command;
 use App\Kafka\Consumers\PaymentConsumer;
 use Junges\Kafka\Facades\Kafka;
 use Illuminate\Support\Facades\Log;
+use App\Services\KafkaConsumerService;
 
 class PaymentConsumerCommand extends Command
 {
     protected $signature = 'kafka:payment-consume';
 
+    public function __construct(
+        private KafkaConsumerService $kafkaConsumer,
+        private PaymentConsumer $consumer
+    ) {
+        parent::__construct();
+    }
+
     public function handle()
     {
         Log::info('Payment consumer started');
 
-        try {
-            $consumer = Kafka::consumer()
-                ->withBrokers(config('kafka.brokers'))
-                ->withConsumerGroupId(config('kafka.consumers.payment_completed.group_id'))
-                ->subscribe(config('kafka.consumers.payment_completed.topic'))
-                ->withHandler(function ($message) {
-                    try {
-                        // Get the message body
-                        $body = $message->getBody();
-
-                        Log::info('Message received from Kafka', [
-                            'body' => $body
-                        ]);
-
-                        (new PaymentConsumer())->handle($message);
-
-                    } catch (Throwable $e) {
-                        Log::error('Handler error: ' . $e->getMessage());
-                    }
-                })
-                ->build();
-
-            $consumer->consume();
-
-        } catch (Throwable $e) {
-            Log::error('Payment Consumer Command error: ' . $e->getMessage());
-        }
+        $this->kafkaConsumer->consume(
+            config('kafka.consumers.payment_completed.group_id'),
+            config('kafka.consumers.payment_completed.topic'),
+            function($message) {
+                $this->consumer->handle($message);
+            }
+        );
     }
 }
