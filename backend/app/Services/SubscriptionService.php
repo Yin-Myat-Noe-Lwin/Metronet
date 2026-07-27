5e846ae6fe08f6061b53530b00c75b22c19c2d46
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-28 06:24:04 +0630

    add function in subscription service to find subscriptions with the customer and plan id

--- backend/app/Services/SubscriptionService.php
+++ backend/app/Services/SubscriptionService.php
@@ -12,4 +12,12 @@ class SubscriptionService
         return Subscription::with('plan')
                             ->findOrFail($subscriptionId);
     }
+
+    public function getCustomerSubscription(int $customerId, int $planId): ?Subscription
+    {
+        return Subscription::where('customer_id', $customerId)
+                            ->where('plan_id', $planId)
+                            ->whereIn('status', [0, 1])
+                            ->first();
+    }
 }
