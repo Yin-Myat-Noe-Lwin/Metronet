95e7de5d97cab740fe4dc040b3ddb209274818d3
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-28 09:31:03 +0630

    refactor plan deactivated consumer

--- backend/app/Console/Commands/PlanDeactivatedConsumerCommand.php
+++ backend/app/Console/Commands/PlanDeactivatedConsumerCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Kafka\Consumers\PlanDeactivatedConsumer;
+use App\Services\KafkaConsumerService;
 use Junges\Kafka\Facades\Kafka;
 use Illuminate\Support\Facades\Log;
 use Throwable;
@@ -12,34 +13,23 @@ class PlanDeactivatedConsumerCommand extends Command
 {
     protected $signature = 'kafka:plan-deactivated-consume';
 
+    public function __construct(
+        private KafkaConsumerService $kafkaConsumer,
+        private PlanDeactivatedConsumer $consumer
+    ) {
+        parent::__construct();
+    }
+
     public function handle()
     {
         Log::info('PlanDeactivatedConsumerCommand started');
 
-        try {
-            $consumer = Kafka::consumer()
-                ->withBrokers(config('kafka.brokers'))
-                ->withConsumerGroupId(config('kakfka.consumers.plan_deactivated.group_id'))
-                ->subscribe(config('kakfka.consumers.plan_deactivated.topic'))
-                ->withHandler(function ($message) {
-                    try {
-                        Log::info('Plan deactivated message received', $message->getBody());
-
-                        (new PlanDeactivatedConsumer())->handle($message);
-
-                        Log::info('Plan deactivated message processed successfully');
-                    } catch (Throwable $e) {
-                        Log::error('Handler error: ' . $e->getMessage());
-                    }
-                })
-                ->build();
-            Log::info('Consumer built successfully');
-
-            $consumer->consume();
-
-        } catch (Throwable $e) {
-            Log::error('Plan Deactivated Consumer Command error: ' . $e->getMessage());
-            return 1;
-        }
+        $this->kafkaConsumer->consume(
+            config('kafka.consumers.plan_deactivated.group_id'),
+            config('kafka.consumers.plan_deactivated.topic'),
+            function($message) {
+                $this->consumer->handle($message);
+            }
+        );
     }
 }
