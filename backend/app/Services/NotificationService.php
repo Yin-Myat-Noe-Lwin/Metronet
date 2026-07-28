50006bbcbbf6e47e6e618dfa7be46d4882b85f5b
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-28 21:23:56 +0630

    fix spelling

--- backend/app/Services/NotificationService.php
+++ backend/app/Services/NotificationService.php
@@ -10,7 +10,7 @@ use Throwable;
 class NotificationService
 {
     public function __construct(
-        NotificationepositoryInterface $notificationRepository
+        NotificationRepositoryInterface $notificationRepository
     ) {
 
     }
