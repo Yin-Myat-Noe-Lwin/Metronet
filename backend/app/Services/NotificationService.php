c1d80533ecc266e0b0521524d7e6ddc7f9b91716
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-28 14:30:40 +0630

    add noti repo interface for noti service

--- backend/app/Services/NotificationService.php
+++ backend/app/Services/NotificationService.php
@@ -4,25 +4,22 @@ namespace App\Services;
 
 use App\Models\Notification;
 use Illuminate\Support\Facades\Log;
+use App\Contracts\Repositories\NotificationRepositoryInterface;
 use Throwable;
 
 class NotificationService
 {
+    public function __construct(
+        NotificationepositoryInterface $notificationepository
+    ) {
+
+    }
+
     public function create(array $data): void
     {
         try {
-            Notification::create([
-                'customer_id' => $data['customer_id'],
-                'event_type' => $data['event_type'],
-                'channel' => $data['channel'],
-                'title' => $data['title'],
-                'message' => $data['message'],
-                'is_read' => 0,
-                'read_at' => null,
-                'scheduled_at' => null,
-                'sent_status' => 1,
-                'sent_at' => now(),
-            ]);
+            $this->notificationepository
+                ->create($data);
 
             Log::info('Notification created', [
                 'customer_id' => $data['customer_id'],
