d718a51e27c7d245f188bf038442212ec9164e0c
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-28 14:07:18 +0630

    add invoice repository interface in invoice service

--- backend/app/Services/InvoiceService.php
+++ backend/app/Services/InvoiceService.php
@@ -3,12 +3,19 @@
 namespace App\Services;
 
 use App\Models\Invoice;
+use App\Contracts\Repositories\InvoiceRepositoryInterface;
 
 class InvoiceService {
 
+    public function __construct (
+        private InvoiceRepositoryInterface $invoiceRepository
+    ) {
+
+    }
+
     public function getInvoiceById(int $id) {
 
-        return Invoice::find($id);
+        return $this->invoiceRepository->getInvoiceById($id);
     }
 
 }
