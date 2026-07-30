b958624f18ff5ed3d0b8a0558f55313c6c091c32
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-31 05:53:04 +0630

    fix class name

--- backend/app/Http/Requests/PlanDiscountRequest.php
+++ backend/app/Http/Requests/PlanDiscountRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class StorePlanDiscountRequest extends FormRequest
+class PlanDiscountRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
