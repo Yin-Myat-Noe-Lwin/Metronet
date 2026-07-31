dc4119aac1b5a98a4b6ce284683e64466c9f406d
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-07-31 07:04:32 +0630

    add payment reminder status in notifciations

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -252,7 +252,7 @@ CREATE TABLE `cpe_assignments` (
 CREATE TABLE `notifications` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `customer_id` BIGINT UNSIGNED NOT NULL,
-    `event_type` TINYINT NOT NULL COMMENT '1=invoice_created, 2=payment_success, 3=subscription_approved, 4=service_activated, 5=subscription_cancelled, 6=plan_updated, 7=plan_deleted 8=cpe_updated, 9=service_area_updated',
+    `event_type` TINYINT NOT NULL COMMENT '1=invoice_created, 2=payment_success, 3=subscription_approved, 4=service_activated, 5=subscription_cancelled, 6=plan_updated, 7=plan_deleted 8=cpe_updated, 9=service_area_updated, 10=payment_reminder',
     `channel` TINYINT NOT NULL DEFAULT 1 COMMENT '1=email, 2=sms, 3=in_app',
     `title` VARCHAR(100) NOT NULL,
     `message` TEXT,
