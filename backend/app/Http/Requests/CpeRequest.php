322ad644b61c897b6aada88eff89137a098b7a5d
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-08-01 07:09:52 +0630

    remove unnecessary word

--- backend/app/Http/Requests/CpeRequest.php
+++ backend/app/Http/Requests/CpeRequest.php
@@ -6,38 +6,30 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CpeRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'serial_number' => 'required|string|max:100|unique:cpes,serial_number',
-            'mac_address'   => [
+
+            'mac_address' => [
                 'required',
                 'string',
                 'max:100',
-                'uniques:cpes,mac_addresses',
+                'unique:cpes,mac_address',
                 'regex:/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/',
-            ]
-
+            ],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'mac_address.regex' => 'Invalid Mac Address'
+            'mac_address.regex' => 'Invalid Mac Address',
         ];
     }
 }
