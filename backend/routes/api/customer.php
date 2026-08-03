eb1188ffb2b56297e33a070570c0785955b24cde
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-08-03 09:52:50 +0630

    add route for reset and forgot password

--- backend/routes/api/customer.php
+++ backend/routes/api/customer.php
@@ -12,6 +12,9 @@
 
   Route::get('/plans', [IspPlanController::class, 'index']);
 
+  Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+  Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+
   Route::middleware('auth:api')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
