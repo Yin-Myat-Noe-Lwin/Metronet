bd9ab746120065ea0f465e4e7bf6c882b1733e1b
Yin-Myat-Noe-Lwin <yinmyatnoelwin1@gmail.com>
2026-08-24 23:25:43 +0630

    update validation messages for updating isp plan

--- backend/app/Http/Requests/IspPlanUpdateRequest.php
+++ backend/app/Http/Requests/IspPlanUpdateRequest.php
@@ -33,7 +33,16 @@ class IspPlanUpdateRequest extends FormRequest
             'price' => 'sometimes|numeric|min:0|max:999999',
             'validity_months' => 'sometimes|integer|min:1|max:12',
             'upload_speed' => 'sometimes|integer|min:1|max:200',
-            'download_speed' => 'sometimes|integer|min:1|max:200',
+            'download_speed' => 'sometimes|integer|min:1|max:200'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return[
+            'validity_months.min' => 'Validity must be at least 1 month',
+            'upload_speed.min' => 'Upload speed must be at least 1 Mbps',
+            'download_speed.min' => 'Download speed must be at least 1 Mbps'
         ];
     }
 }
